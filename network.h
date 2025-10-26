1f641f033c6f2926a5f541651b85931b5bc2adf1
k1ng0k <sarvajadam0@gmail.com>
2025-10-26 11:22:33 +0100

    generating custom functions

---
+++ network.h
@@ -0,0 +1,38 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <stddef.h>
+
+typedef struct Device {
+    char id[64];
+    char ip[32];
+    char mac[32];
+    char type[16];
+    char vendor[64];
+    struct Device *next;
+} Device;
+
+typedef struct {
+    Device *head;
+    int count;
+} DeviceList;
+
+DeviceList* createDeviceList(void);
+void add_dev(DeviceList *list, const char *id, const char *ip,const char *mac, const char *type, const char *vendor);
+Device* find_dev(DeviceList *list, const char *ip);
+void remove_dev(DeviceList *list, const char *ip);
+void free_devlist(DeviceList *list);
+
+int initNetworkScan(const char *interface);
+int scanNetwork(DeviceList *list, const char *subnet);
+int pingDevice(const char *ip);
+int getMACAddress(const char *ip, char *macBuffer, size_t bufferSize);
+int querySNMP(const char *ip, char *vendorBuffer, size_t bufferSize);
+int discoverNeighbors(DeviceList *list, const char *ip);
+void shutdownNetworkScan(void);
+
+// === Import/Export ===
+//int saveNetworkToFile(const DeviceList *list, const char *filename);
+//int loadNetworkFromFile(DeviceList *list, const char *filename);
+
+#endif
---
+++ table.h
@@ -0,0 +1,10 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "network.h"
+
+void print_table(const DeviceList *list);
+//void exportTableCSV(const DeviceList *list, const char *filename);
+//void exportTableJSON(const DeviceList *list, const char *filename);
+
+#endif
